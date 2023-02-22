ef6aaf451d34f4b6794ae45b8b6e8e59314ebbf8
Margarita <margaritakrasnauska@gmail.com>
2023-02-22 11:39:49 +0000

    Add project files.

---
+++ Additional_Features/CompanyUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    public class CompanyUser
+    {
+        public CompanyUser(string name) : this(name, username, SecretQuestion, secret, "Jlk.05235.22")
+        {
+
+        }
+        public CompanyUser(string name, string username, string question, string SecretQuestion, string pass)
+        {
+
+
+            Name = name;
+            UserName = username;
+            UserPassword = pass;
+
+            SecretAnswer = secret;
+        }
+
+        const string SecretQuestion = "What is your mothers maiden name?";
+        string SecretAnswer { get; set; }
+        string Name { get; set; }
+        string UserName { get; set; }
+        string UserPassword { get; set; }
+        public static string username { get; private set; }
+        public static string secret { get; private set; }
+    }
+}
---
+++ Additional_Features/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var NewUser = new CompanyUser("David");
+            var NewUserA = new CompanyUser("Sarah");
+
+        }
+    }
+}
